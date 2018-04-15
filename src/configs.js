48a460d8469236d0d7a907b75a41abaab5a93969
eufat <eutsat@gmail.com>
2018-04-15 14:36:37 +0700

    Add main-auth

---
+++ src/configs.js
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAunkgswJoWW_cN8iF5SWDXKfEm07scMuo",
+  authDomain: "mqtt-remote.firebaseapp.com",
+  databaseURL: "https://mqtt-remote.firebaseio.com",
+  storageBucket: "mqtt-remote.appspot.com",
+  messagingSenderId: "369821906057"
+};
--- src/main-app.js
+++ src/main-app.js
@@ -1,6 +1,7 @@
 import { PolymerElement, html } from "@polymer/polymer/polymer-element";
 import "@polymer/app-layout/demo/sample-content";
 import "./main-dashboard";
+import "./main-auth";
 
 class MainApp extends PolymerElement {
   static get properties() {
@@ -11,14 +12,16 @@ class MainApp extends PolymerElement {
     return html`
       <style>
       </style>
+      <main-auth />
+      <!--
       <main-dashboard>
         <span slot="app-content">
-          App content
         </span>
         <span slot="drawer-content">
           Drawer content
         </span>
       </main-dashboard>
+      -->
     `;
   }
 }
