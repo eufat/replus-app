5a9cadce86157d330a3f062e66cb3da7f8fe6ca6
Eufrat Tsaqib <eutsat@gmail.com>
2018-05-08 19:01:45 +0700

    Fix polymer serving

--- src/components/main-app.js
+++ src/components/main-app.js
@@ -6,7 +6,6 @@ import "@polymer/app-layout/app-scroll-effects/effects/waterfall.js";
 import "@polymer/app-layout/app-toolbar/app-toolbar.js";
 import { setPassiveTouchGestures } from "@polymer/polymer/lib/utils/settings.js";
 
-import { menuIcon } from "./my-icons.js";
 import "./snack-bar.js";
 
 import { connect } from "pwa-helpers/connect-mixin.js";
@@ -23,72 +22,12 @@ import {
     updateLayout
 } from "../actions/app.js";
 
-class MyApp extends connect(store)(LitElement) {
+class MainApp extends connect(store)(LitElement) {
     _render({ appTitle, _page, _drawerOpened, _snackbarOpened, _offline }) {
         // Anything that's related to rendering should be done in here.
         return html`
     <style>
-      :host {
-        --app-drawer-width: 256px;
-        display: block;
-
-        --app-primary-color: #E91E63;
-        --app-secondary-color: #293237;
-        --app-dark-text-color: var(--app-secondary-color);
-        --app-light-text-color: white;
-        --app-section-even-color: #f7f7f7;
-        --app-section-odd-color: white;
-
-        --app-header-background-color: white;
-        --app-header-text-color: var(--app-dark-text-color);
-        --app-header-selected-color: var(--app-primary-color);
-
-        --app-drawer-background-color: var(--app-secondary-color);
-        --app-drawer-text-color: var(--app-light-text-color);
-        --app-drawer-selected-color: #78909C;
-      }
-
-      app-header {
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        text-align: center;
-        background-color: var(--app-header-background-color);
-        color: var(--app-header-text-color);
-        border-bottom: 1px solid #eee;
-      }
-
-      .toolbar-top {
-        background-color: var(--app-header-background-color);
-      }
-
-      [main-title] {
-        font-family: 'Pacifico';
-        text-transform: lowercase;
-        font-size: 30px;
-        /* In the narrow layout, the toolbar is offset by the width of the
-        drawer button, and the text looks not centered. Add a padding to
-        match that button */
-        padding-right: 44px;
-      }
-
-      .toolbar-list {
-        display: none;
-      }
-
-      .toolbar-list > a {
-        display: inline-block;
-        color: var(--app-header-text-color);
-        text-decoration: none;
-        line-height: 30px;
-        padding: 4px 24px;
-      }
-
-      .toolbar-list > a[selected] {
-        color: var(--app-header-selected-color);
-        border-bottom: 4px solid var(--app-header-selected-color);
-      }
+      :host {}
 
       .menu-btn {
         background: none;
@@ -132,42 +71,13 @@ class MyApp extends connect(store)(LitElement) {
       .page[active] {
         display: block;
       }
-
-      footer {
-        padding: 24px;
-        background: var(--app-drawer-background-color);
-        color: var(--app-drawer-text-color);
-        text-align: center;
-      }
-
-      /* Wide layout: when the viewport width is bigger than 460px, layout
-      changes to a wide layout. */
-      @media (min-width: 460px) {
-        .toolbar-list {
-          display: block;
-        }
-
-        .menu-btn {
-          display: none;
-        }
-
-        .main-content {
-          padding-top: 107px;
-        }
-
-        /* The drawer button isn't shown in the wide layout, so we don't
-        need to offset the title */
-        [main-title] {
-          padding-right: 0px;
-        }
-      }
     </style>
 
     <!-- Header -->
     <app-header condenses reveals effects="waterfall">
       <app-toolbar class="toolbar-top">
         <button class="menu-btn" title="Menu" on-click="${_ =>
-            store.dispatch(updateDrawerState(true))}">${menuIcon}</button>
+            store.dispatch(updateDrawerState(true))}">Menu</button>
         <div main-title>${appTitle}</div>
       </app-toolbar>
 
@@ -257,4 +167,4 @@ class MyApp extends connect(store)(LitElement) {
     }
 }
 
-window.customElements.define("my-app", MyApp);
+window.customElements.define("main-app", MainApp);
