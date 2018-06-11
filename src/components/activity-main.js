c5263666195620ee076e1bbcfb2708a36fe9cd7c
Eufrat Tsaqib <eutsat@gmail.com>
2018-06-11 10:18:42 +0700

    Add activity view

--- src/components/activity-main.js
+++ src/components/activity-main.js
@@ -1,5 +1,6 @@
 import {LitElement, html} from '@polymer/lit-element';
 import '@polymer/paper-card/paper-card.js';
+import '@polymer/paper-material/paper-material.js';
 
 import {env} from '../configs.js';
 import {getDateFromFilename} from '../utils.js';
@@ -95,7 +96,96 @@ export default class activityMain extends LitElement {
                     width: 480px;
                     margin-bottom: 20px;
                 }
+
+                .activities-listing::before {
+                    position: absolute;
+                    top: 0;
+                    bottom: 0;
+                    left: 14px;
+                    z-index: -1;
+                    display: block;
+                    width: 2px;
+                    content: "";
+                    background-color: #eff1f3;
+                }
+
+                .activities-listing-padded {
+                    padding-left: 39px;
+                }
+
+                .activities-listing {
+                    position: relative;
+                    padding-bottom: 20px;
+                    margin-bottom: 15px;
+                }
+
+                .activity-group-title {
+                    margin-top: 15px;
+                    margin-left: -31px;
+                    color: #586069;
+                }
+
+                .activity-group-title .time-icon-activity {
+                    margin-right: 17px;
+                    color: #c6cbd1;
+                    background: #fafafa;
+                }
+
+                svg:not(:root) {
+                    overflow: hidden;
+                }
+
+                .time-icon {
+                    display: inline-block;
+                    vertical-align: text-bottom;
+                    fill: currentColor;
+                }
+
+                .test {
+                    display: block;
+                    height: 100px;
+                }
+
+                .container {
+                    margin: 0 auto;
+                    max-width: 960px;
+                }
+
+                paper-material {
+                    display: block;
+                    margin: 20px;
+                    margin-left: 0;
+                    padding: 10px 20px;
+                    background-color: white;
+                }
+
+                .activity-group {
+                    margin-top: 10px;
+                    list-style-type: none;
+                }
             </style>
+            <div class="container">
+                <div class="activities-listing activities-listing-padded">
+                    <div class="activity-group-title">
+                        <svg class="time-icon time-icon-activity" viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M10.86 7c-.45-1.72-2-3-3.86-3-1.86 0-3.41 1.28-3.86 3H0v2h3.14c.45 1.72 2 3 3.86 3 1.86 0 3.41-1.28 3.86-3H14V7h-3.14zM7 10.2c-1.22 0-2.2-.98-2.2-2.2 0-1.22.98-2.2 2.2-2.2 1.22 0 2.2.98 2.2 2.2 0 1.22-.98 2.2-2.2 2.2z"></path></svg>Activity on Jun 11, 2018
+                    </div>
+                    <paper-material class="activity-group" elevation="0">
+                        <div class="test"></div>
+                    </paper-material>
+                    <div class="activity-group-title">
+                        <svg class="time-icon time-icon-activity" viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M10.86 7c-.45-1.72-2-3-3.86-3-1.86 0-3.41 1.28-3.86 3H0v2h3.14c.45 1.72 2 3 3.86 3 1.86 0 3.41-1.28 3.86-3H14V7h-3.14zM7 10.2c-1.22 0-2.2-.98-2.2-2.2 0-1.22.98-2.2 2.2-2.2 1.22 0 2.2.98 2.2 2.2 0 1.22-.98 2.2-2.2 2.2z"></path></svg>Activity on Jun 11, 2018
+                    </div>
+                    <paper-material class="activity-group" elevation="0">
+                        <div class="test"></div>
+                    </paper-material>
+                    <div class="activity-group-title">
+                        <svg class="time-icon time-icon-activity" viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M10.86 7c-.45-1.72-2-3-3.86-3-1.86 0-3.41 1.28-3.86 3H0v2h3.14c.45 1.72 2 3 3.86 3 1.86 0 3.41-1.28 3.86-3H14V7h-3.14zM7 10.2c-1.22 0-2.2-.98-2.2-2.2 0-1.22.98-2.2 2.2-2.2 1.22 0 2.2.98 2.2 2.2 0 1.22-.98 2.2-2.2 2.2z"></path></svg>Activity on Jun 11, 2018
+                    </div>
+                    <paper-material class="activity-group" elevation="0">
+                        <div class="test"></div>
+                    </paper-material>
+                </div>
+            </div>
             <div>
                 <p class="event-container">Listening on: ${realtimeURL}</p>
                 <p class="event-container">Connection status: ${realtimeStatus}</p>
