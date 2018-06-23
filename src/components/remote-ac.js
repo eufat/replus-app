a6ebb91457123bdd74229b62c37587e26c507d36
arfi.renaldi <arfirenaldi@gmail.com>
2018-06-23 15:38:16 +0700

    test remote ac function with properties

--- src/components/remote-ac.js
+++ src/components/remote-ac.js
@@ -117,34 +117,196 @@ class RemoteAc extends PolymerElement {
         `;
     }
 
-    // static get properties() {
-    //     return {
-    //         fans: {
-    //             type: Array,
-    //             value: ['Auto', 'Low', 'Medium', 'High']
-    //         },
-    //         modes: {
-    //             type: Array,
-    //             value: ['Auto', 'Cool', 'Dry', 'Heat']
-    //         },
-    //         remote: {
-    //             type: String,
-    //             observer: '_changeRemote',
-    //         },
-    //         mode: {type: Number, observer: '_changeMode'},
-    //         fan: {type: Number, observer: '_changeFan'},
-    //     };
-    // }
+    static get properties() {
+        return {
+            fans: {
+                type: Array,
+                value: ['Auto', 'Low', 'Medium', 'High'],
+            },
+            modes: {
+                type: Array,
+                value: ['Auto', 'Cool', 'Dry', 'Heat'],
+            },
+            remote: {
+                type: String,
+                observer: '_changeRemote',
+            },
+            mode: {type: Number, observer: '_changeMode'},
+            fan: {type: Number, observer: '_changeFan'},
 
-    // ready() {
-    //     super.ready();
-    //     const thisRemoteAC = this;
-    //     thisRemoteAC.setupPosition();
-    //     thisRemoteAC.stateInitial();
-    //     window.addEventListener('resize', function(event) {
-    //         thisRemoteAC.setupPosition();
-    //     });
-    // }
+            manifestFans: {type: Array, value: [0, 1, 2, 3]},
+            manifestModes: {type: Array, value: [1, 2]},
+            manifestTemps: {type: Array, value: [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]},
+
+            tempIndex: {type: Number, value: null},
+            fanIndex: {type: Number, value: 0},
+            modeIndex: {type: Number, value: 0},
+        };
+    }
+
+    ready() {
+        super.ready();
+        const thisRemoteAC = this;
+        thisRemoteAC.setupPosition();
+        thisRemoteAC.stateInitial();
+        window.addEventListener('resize', () => {
+            thisRemoteAC.setupPosition();
+        });
+
+        thisRemoteAC._changeMode();
+        thisRemoteAC._changeFan();
+        thisRemoteAC.parseFans();
+        thisRemoteAC.parseTemps();
+    }
+
+    static get observers() {
+        return [
+            'parseFans(mode)',
+            'parseTemps(mode, fan)',
+            'resetTimeout(mode, fan, temp)',
+        ];
+    }
+
+    parseFans() {
+        const thisRemoteAC = this;
+        // thisRemoteAC.manifestFans = [];
+        // let mode = thisRemoteAC.manifest[thisRemoteAC.mode];
+        // for (let fan in mode) {
+        //     if (mode.hasOwnProperty(fan)) {
+        //         thisRemoteAC.push('manifestFans', parseInt(fan));
+        //     }
+        // }
+        thisRemoteAC.fanIndex = 0;
+        thisRemoteAC.fan = thisRemoteAC.manifestFans[thisRemoteAC.fanIndex];
+        thisRemoteAC.mode = thisRemoteAC.manifestModes[thisRemoteAC.modeIndex];
+    }
+
+    parseTemps() {
+        const thisRemoteAC = this;
+        // thisRemoteAC.manifestTemps = thisRemoteAC.manifest[thisRemoteAC.mode][thisRemoteAC.fan];
+        if (typeof thisRemoteAC.manifestTemps[thisRemoteAC.tempIndex] == 'undefined') {
+            thisRemoteAC.tempIndex = 0;
+            thisRemoteAC.temp = thisRemoteAC.manifestTemps[thisRemoteAC.tempIndex];
+        }
+    }
+
+    resetTimeout() {
+        const thisRemoteAC = this;
+        clearTimeout(thisRemoteAC.timeout);
+        thisRemoteAC.timeout = setTimeout(() => {
+            thisRemoteAC.send();
+        }, 1000);
+    }
+
+    _changeMode() {
+        const thisRemoteAC = this;
+        thisRemoteAC.$.displayMode.innerHTML = thisRemoteAC.modes[thisRemoteAC.mode];
+    }
+
+    _changeFan() {
+        const thisRemoteAC = this;
+        thisRemoteAC.$.displayFan.innerHTML = thisRemoteAC.fans[thisRemoteAC.fan];
+    }
+
+    _changeRemote() {
+        const thisRemoteAC = this;
+        let jenis = thisRemoteAC.remote.substring(0, 2);
+        thisRemoteAC.brand = thisRemoteAC.remote.substring(3).toLowerCase();
+        if (jenis == 'AC') {
+            thisRemoteAC.stateInitial();
+            thisRemoteAC.$.ajaxManifest.generateRequest();
+        }
+    }
+
+    setupPosition() {
+        const thisRemoteAC = this;
+        if (window.innerHeight > 470) thisRemoteAC.$.remoteContainer.style.marginTop = window.innerHeight - (64+20+120+270) + 'px';
+        else thisRemoteAC.$.remoteContainer.style.marginTop = '10px';
+
+        if (window.innerWidth > 350) {
+            thisRemoteAC.$.mainContainer.style.width = '330px';
+            thisRemoteAC.$.mainContainer.style.marginLeft = (window.innerWidth - 330)/2 + 'px';
+            // if (window.innerWidth > 640) {
+            //     thisRemoteAC.$.mainContainer.style.marginLeft = ((window.innerWidth - 330)/2 - 128) + 'px';
+            // }
+        } else {
+            thisRemoteAC.$.mainContainer.style.width = '250px';
+            thisRemoteAC.$.mainContainer.style.marginLeft = (window.innerWidth - 250)/2 + 'px';
+        }
+    }
+
+    stateInitial() {
+        const thisRemoteAC = this;
+        thisRemoteAC.switchedON = false;
+        thisRemoteAC.$.displayContainer.style.visibility = 'hidden';
+        thisRemoteAC.$.btnPower.setAttribute('icon', 'power-settings-new');
+        thisRemoteAC.$.btnMode.disabled = 'true';
+        thisRemoteAC.$.btnFan.disabled = 'true';
+        thisRemoteAC.$.btnTempUp.disabled = 'true';
+        thisRemoteAC.$.btnTempDown.disabled = 'true';
+    }
+
+    _tapPower() {
+        const thisRemoteAC = this;
+        if (thisRemoteAC.switchedON) {
+            thisRemoteAC.stateInitial();
+            thisRemoteAC._tapPowerOFF();
+        } else {
+            thisRemoteAC.stateEnabled();
+            // thisRemoteAC.send();
+        }
+    }
+
+    _tapPowerOFF() {
+        const thisRemoteAC = this;
+        thisRemoteAC.command = thisRemoteAC.brand + '-0000';
+        // thisRemoteAC.$.ajax.generateRequest();
+        // thisRemoteAC.parseManifest();
+        thisRemoteAC.temp = 18;
+    }
+
+    stateEnabled() {
+        const thisRemoteAC = this;
+        thisRemoteAC.switchedON = true;
+        thisRemoteAC.$.displayContainer.style.visibility = 'visible';
+        thisRemoteAC.$.btnPower.setAttribute('icon', 'close');
+        thisRemoteAC.$.btnMode.removeAttribute('disabled');
+        thisRemoteAC.$.btnFan.removeAttribute('disabled');
+        thisRemoteAC.$.btnTempUp.removeAttribute('disabled');
+        thisRemoteAC.$.btnTempDown.removeAttribute('disabled');
+    }
+
+    send() {
+        const thisRemoteAC = this;
+        thisRemoteAC.command = thisRemoteAC.brand + '-' + thisRemoteAC.mode + thisRemoteAC.fan + thisRemoteAC.temp;
+        // if (thisRemoteAC.switchedON) thisRemoteAC.$.ajax.generateRequest();
+    }
+
+    _tapMode() {
+        const thisRemoteAC = this;
+        if (thisRemoteAC.modeIndex < thisRemoteAC.manifestModes.length - 1) thisRemoteAC.modeIndex++;
+        else thisRemoteAC.modeIndex = 0;
+        thisRemoteAC.mode = thisRemoteAC.manifestModes[thisRemoteAC.modeIndex];
+    }
+
+    _tapFan() {
+        const thisRemoteAC = this;
+        if (thisRemoteAC.fanIndex < thisRemoteAC.manifestFans.length - 1) thisRemoteAC.fanIndex++;
+        else thisRemoteAC.fanIndex = 0;
+        thisRemoteAC.fan = thisRemoteAC.manifestFans[thisRemoteAC.fanIndex];
+    }
+
+    _tapUp() {
+        const thisRemoteAC = this;
+        if (thisRemoteAC.tempIndex < thisRemoteAC.manifestTemps.length - 1) thisRemoteAC.tempIndex++;
+        thisRemoteAC.temp = thisRemoteAC.manifestTemps[thisRemoteAC.tempIndex];
+    }
+
+    _tapDown() {
+        const thisRemoteAC = this;
+        if (thisRemoteAC.tempIndex > 0) thisRemoteAC.tempIndex--;
+        thisRemoteAC.temp = thisRemoteAC.manifestTemps[thisRemoteAC.tempIndex];
+    }
 
     // parseManifest() {
     //     const thisRemoteAC = this;
@@ -157,128 +319,6 @@ class RemoteAc extends PolymerElement {
     //     }
     //     thisRemoteAC.modeIndex = 0;
     //     thisRemoteAC.mode = thisRemoteAC.manifestModes[thisRemoteAC.modeIndex];
-    // }
-
-    // parseFans() {
-    //     thisRemoteAC.manifestFans = [];
-    //     var mode = thisRemoteAC.manifest[thisRemoteAC.mode];
-    //     for (var fan in mode) {
-    //         if (mode.hasOwnProperty(fan)) {
-    //             thisRemoteAC.push('manifestFans', parseInt(fan));
-    //         }
-    //     }
-    //     thisRemoteAC.fanIndex = 0;
-    //     thisRemoteAC.fan = thisRemoteAC.manifestFans[thisRemoteAC.fanIndex];
-    // }
-
-    // parseTemps() {
-    //     thisRemoteAC.manifestTemps = thisRemoteAC.manifest[thisRemoteAC.mode][thisRemoteAC.fan];
-    //     if (typeof thisRemoteAC.manifestTemps[thisRemoteAC.tempIndex] == 'undefined') {
-    //         thisRemoteAC.tempIndex = 0;
-    //         thisRemoteAC.temp = thisRemoteAC.manifestTemps[thisRemoteAC.tempIndex];
-    //     }
-    // }
-
-    // resetTimeout() {
-    //     clearTimeout(thisRemoteAC.timeout);
-    //     thisRemoteAC.timeout = setTimeout(() => { thisRemoteAC.send(); }, 1000);
-    // }
-
-    // stateInitial() {
-    //     thisRemoteAC.switchedON = false;
-    //     thisRemoteAC.$.displayContainer.style.visibility = "hidden";
-    //     thisRemoteAC.$.btnPower.setAttribute("icon", "power-settings-new");
-    //     thisRemoteAC.$.btnMode.disabled = "true";
-    //     thisRemoteAC.$.btnFan.disabled = "true";
-    //     thisRemoteAC.$.btnTempUp.disabled = "true";
-    //     thisRemoteAC.$.btnTempDown.disabled = "true";
-    // }
-
-    // stateEnabled() {
-    //     thisRemoteAC.switchedON = true;
-    //     thisRemoteAC.$.displayContainer.style.visibility = "visible";
-    //     thisRemoteAC.$.btnPower.setAttribute("icon", "close");
-    //     thisRemoteAC.$.btnMode.removeAttribute('disabled');
-    //     thisRemoteAC.$.btnFan.removeAttribute('disabled');
-    //     thisRemoteAC.$.btnTempUp.removeAttribute('disabled');
-    //     thisRemoteAC.$.btnTempDown.removeAttribute('disabled');
-    // }
-
-    // send() {
-    //     thisRemoteAC.command = thisRemoteAC.brand + "-" + thisRemoteAC.mode + thisRemoteAC.fan + thisRemoteAC.temp;
-    //     if (thisRemoteAC.switchedON) thisRemoteAC.$.ajax.generateRequest();
-    // }
-
-    // setupPosition() {
-    //     if (window.innerHeight > 470) thisRemoteAC.$.remoteContainer.style.marginTop = window.innerHeight - (64+20+120+270) + 'px';
-    //     else thisRemoteAC.$.remoteContainer.style.marginTop = '10px';
-
-    //     if (window.innerWidth > 350) {
-    //         thisRemoteAC.$.mainContainer.style.width = '330px';
-    //         thisRemoteAC.$.mainContainer.style.marginLeft = (window.innerWidth - 330)/2 + 'px';
-    //         // if (window.innerWidth > 640) {
-    //         //     thisRemoteAC.$.mainContainer.style.marginLeft = ((window.innerWidth - 330)/2 - 128) + 'px';
-    //         // }
-    //     } else {
-    //         thisRemoteAC.$.mainContainer.style.width = '250px';
-    //         thisRemoteAC.$.mainContainer.style.marginLeft = (window.innerWidth - 250)/2 + 'px';
-    //     }
-    // }
-
-    // _changeMode() {
-    //     thisRemoteAC.$.displayMode.innerHTML = thisRemoteAC.modes[thisRemoteAC.mode];
-    // }
-
-    // _changeFan() {
-    //     thisRemoteAC.$.displayFan.innerHTML = thisRemoteAC.fans[thisRemoteAC.fan];
-    // }
-
-    // _changeRemote() {
-    //     var jenis = thisRemoteAC.remote.substring(0, 2);
-    //     thisRemoteAC.brand = thisRemoteAC.remote.substring(3).toLowerCase();
-    //     if (jenis == "AC") {
-    //         thisRemoteAC.stateInitial();
-    //         thisRemoteAC.$.ajaxManifest.generateRequest();
-    //     }
-    // }
-
-    // _tapPower() {
-    //     if (thisRemoteAC.switchedON) {
-    //         thisRemoteAC.stateInitial();
-    //         thisRemoteAC._tapPowerOFF();
-    //     } else {
-    //         thisRemoteAC.stateEnabled();
-    //         thisRemoteAC.send();
-    //     }
-    // }
-
-    // _tapPowerOFF() {
-    //     thisRemoteAC.command = thisRemoteAC.brand + "-0000";
-    //     thisRemoteAC.$.ajax.generateRequest();
-    //     thisRemoteAC.parseManifest();
-    //     thisRemoteAC.temp = 18;
-    // }
-
-    // _tapMode() {
-    //     if (thisRemoteAC.modeIndex < thisRemoteAC.manifestModes.length - 1) thisRemoteAC.modeIndex++;
-    //     else thisRemoteAC.modeIndex = 0;
-    //     thisRemoteAC.mode = thisRemoteAC.manifestModes[thisRemoteAC.modeIndex];
-    // }
-
-    // _tapFan() {
-    //     if (thisRemoteAC.fanIndex < thisRemoteAC.manifestFans.length - 1) thisRemoteAC.fanIndex++;
-    //     else thisRemoteAC.fanIndex = 0;
-    //     thisRemoteAC.fan = thisRemoteAC.manifestFans[thisRemoteAC.fanIndex];
-    // }
-
-    // _tapUp() {
-    //     if (thisRemoteAC.tempIndex < thisRemoteAC.manifestTemps.length - 1) thisRemoteAC.tempIndex++;
-    //     thisRemoteAC.temp = thisRemoteAC.manifestTemps[thisRemoteAC.tempIndex];
-    // }
-
-    // _tapDown() {
-    //     if (thisRemoteAC.tempIndex > 0) thisRemoteAC.tempIndex--;
-    //     thisRemoteAC.temp = thisRemoteAC.manifestTemps[thisRemoteAC.tempIndex];
     // }
 
     // _handleResponse() {
