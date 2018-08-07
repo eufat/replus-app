22cbcb034f8e5b1a227b4de5f44f5239b0e92d83
arfi.renaldi <arfirenaldi@gmail.com>
2018-08-07 22:29:42 +0700

    add statistic in account

--- src/components/main-account.js
+++ src/components/main-account.js
@@ -22,12 +22,14 @@ export default class MainAccount extends connect(store)(LitElement) {
             currentUser: Object,
             active: Boolean,
             provider: String,
+            rooms: Array,
         };
     }
 
     constructor() {
         super();
         this.currentUser = {};
+        this.rooms = [];
     }
 
     _shouldRender(props, changedProps, old) {
@@ -36,6 +38,7 @@ export default class MainAccount extends connect(store)(LitElement) {
 
     _stateChanged(state) {
         this.currentUser = get(state, 'app.currentUser');
+        this.rooms = get(state, 'remote.rooms');
     }
 
     _didRender() {
@@ -48,9 +51,22 @@ export default class MainAccount extends connect(store)(LitElement) {
         }
     }
 
-    _render({currentUser, provider}) {
+    _render({currentUser, provider, rooms}) {
         const providerIsGoogle = provider === undefined ? true : provider === 'google.com';
         const providerIsFacebook = provider === undefined ? true : provider === 'facebook.com';
+
+        let totalDevice = 0;
+        let totalRemote = 0;
+        const deviceValues = _.values(rooms);
+        deviceValues.map((deviceItem) => {
+            if (deviceItem.devices.length != 0) {
+                totalDevice = totalDevice + deviceItem.devices.length;
+            }
+            if (deviceItem.remotes.length != 0) {
+                totalRemote = totalRemote + deviceItem.remotes.length;
+            }
+        });
+        console.log(totalRemote);
 
         return html`
             <style>
@@ -72,8 +88,48 @@ export default class MainAccount extends connect(store)(LitElement) {
                     --mdc-theme-on-secondary: black;
                     --mdc-theme-secondary: white;
                 }
+
+                * {
+                    box-sizing: border-box;
+                }
+
+                /* Create three equal columns that floats next to each other */
+                .column {
+                    float: left;
+                    width: 33.33%;
+                }
+
+                /* Clear floats after the columns */
+                .row:after {
+                    content: "";
+                    display: table;
+                    clear: both;
+                }
+                .column p {
+                    text-align: center;
+                }
+                .total {
+                    margin-bottom: 0px !important;
+                }
+                .title {
+                    margin-top: 0px !important;
+                }
             </style>
             <div role="listbox" class="settings">
+                <div class="row">
+                    <div class="column">
+                        <p class="total">${rooms.length}</p>
+                        <p class="title">Rooms</p>
+                    </div>
+                    <div class="column">
+                        <p class="total">${totalRemote}</p>
+                        <p class="title">Remotes</p>
+                    </div>
+                    <div class="column">
+                        <p class="total">${totalDevice}</p>
+                        <p class="title">Devices</p>
+                    </div>
+                </div>
                 <paper-item>
                     <paper-item-body class="text-container">
                         <p class="left">Display Name</p>
