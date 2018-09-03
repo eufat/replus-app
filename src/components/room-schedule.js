b7fabf7e4964d1c1dd00c9e3aef3953254ebccb8
Eufrat Tsaqib <eutsat@gmail.com>
2018-09-03 14:55:59 +0700

    Fix #20 on title casing the remote name

--- src/components/room-schedule.js
+++ src/components/room-schedule.js
@@ -30,7 +30,7 @@ import '@material/mwc-button';
 import '@material/mwc-icon';
 
 import {createSchedule, fetchIR, removeSchedule} from '../actions/remote.js';
-import {mobileCheck} from '../utils.js';
+import {mobileCheck, toTitleCase} from '../utils.js';
 import {store} from '../store.js';
 
 const get = _.get;
@@ -414,8 +414,7 @@ class AddSchedule extends connect(store)(PolymerElement) {
         let stateRemotes = get(state, 'remote.activeRoom.remotes') || [];
         stateRemotes = stateRemotes.map((remote) => {
             const name = get(remote, 'name');
-            const nameUpperCased = name.toUpperCase();
-            return nameUpperCased;
+            return toTitleCase(name);
         });
         this.remotes = stateRemotes;
         this.manifest = get(state, 'remote.manifest');
