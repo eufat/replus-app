c9b83ac51fd12e3e77f7482542802331b2ad9d7c
arfi.renaldi <arfirenaldi@gmail.com>
2019-01-03 12:38:42 +0700

    fix #26 manifest

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,7 @@
 /* eslint no-console: ["error", { allow: ["info"] }] */
 
+self.addEventListener('fetch', function(event) {});
+
 console.info(
     'Service worker disabled for development, will be generated at build time.'
 );
